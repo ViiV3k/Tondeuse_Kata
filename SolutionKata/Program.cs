a431f3275f84f93721fda95525f360714066a587
Vivek Kumar Bhuttoo <vivek.bhuttoo@vrconseil.com>
2023-10-04 16:16:56 +0200

    Solution to KATA

---
+++ SolutionKata/Program.cs
@@ -0,0 +1,35 @@
+﻿// Lire le fichier d'entrée
+using SolutionKata;
+
+string[] lines = File.ReadAllLines("input.txt");
+
+// Parser les coordonnées de la pelouse
+string[] gridSize = lines[0].Split(' ');
+int maxX = int.Parse(gridSize[0]);
+int maxY = int.Parse(gridSize[1]);
+
+List<Tondeuse> tondeuses = new List<Tondeuse>();
+
+// Parcourir les lignes d'instructions pour chaque tondeuse
+for (int i = 1; i < lines.Length; i += 2)
+{
+    string[] initialPosition = lines[i].Split(' ');
+    int initialX = int.Parse(initialPosition[0]);
+    int initialY = int.Parse(initialPosition[1]);
+    char initialOrientation = char.Parse(initialPosition[2]);
+
+    Tondeuse tondeuse = new Tondeuse(initialX, initialY, initialOrientation);
+    string instructions = lines[i + 1];
+
+    // Exécuter les instructions
+    tondeuse.ExecuteInstructions(instructions, maxX, maxY);
+
+    // Ajouter la tondeuse à la liste
+    tondeuses.Add(tondeuse);
+}
+
+// Afficher la position finale de chaque tondeuse
+foreach (var tondeuse in tondeuses)
+{
+    Console.WriteLine($"{tondeuse.X} {tondeuse.Y} {tondeuse.Orientation}");
+}
---
+++ SolutionKata/Tondeuse.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolutionKata
+{
+    internal class Tondeuse
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public char Orientation { get; private set; }
+
+        public Tondeuse(int x, int y, char orientation)
+        {
+            X = x;
+            Y = y;
+            Orientation = orientation;
+        }
+
+        public void ExecuteInstructions(string instructions, int maxX, int maxY)
+        {
+            foreach (char instruction in instructions)
+            {
+                if (instruction == 'G')                
+                    RotateLeft();
+                
+                else if (instruction == 'D')                
+                    RotateRight();
+                
+                else if (instruction == 'A')                
+                    MoveForward(maxX, maxY);
+                
+            }
+        }
+
+        private void RotateLeft()
+        {
+            switch (Orientation)
+            {
+                case 'N':
+                    Orientation = 'W';
+                    break;
+
+                case 'E':
+                    Orientation = 'N';
+                    break;
+
+                case 'S':
+                    Orientation = 'E';
+                    break;
+
+                case 'W':
+                    Orientation = 'S';
+                    break;
+            }
+        }
+
+        private void RotateRight()
+        {
+            switch (Orientation)
+            {
+                case 'N':
+                    Orientation = 'E';
+                    break;
+
+                case 'E':
+                    Orientation = 'S';
+                    break;
+
+                case 'S':
+                    Orientation = 'W';
+                    break;
+
+                case 'W':
+                    Orientation = 'N';
+                    break;
+            }
+        }
+
+        private void MoveForward(int maxX, int maxY)
+        {
+            switch (Orientation)
+            {
+                case 'N':
+                    if (Y < maxY)                    
+                        Y++;
+                    
+                    break;
+                case 'E':
+                    if (X < maxX)
+                    
+                        X++;
+                    
+                    break;
+                case 'S':
+                    if (Y > 0)                    
+                        Y--;
+                    
+                    break;
+                case 'W':
+                    if (X > 0)                    
+                        X--;
+                    
+                    break;
+            }
+        }
+    }
+}
